d3f0631a865e1efee5c5dc1137a90a1a9a5f956c
mauricedibbets1986 <mauricedibbets1986@gmail.com>
2020-02-03 13:47:13 +0100

    “backup git add . git remote add origin https://github.com/mauricedibbets1986/IntroQien2020.git
    q
    ;

--- hallo.java
+++ hallo.java
@@ -1,2 +1,18 @@
-hallo
-Nog een lijn
+class hallo {
+	public static void main(String[] args) {
+		System.out.println("Hallo");
+		int mijnVar;
+		// mijnVar = 4;
+		mijnVar = 6;
+		System.out.println(mijnVar);
+		String naam = "Maurice";
+		char letter = 'H';
+		char letter2 = 73;
+		voordoen();
+	}
+
+	static void voordoen() {
+		System.out.println("hoi");
+	}
+
+}
